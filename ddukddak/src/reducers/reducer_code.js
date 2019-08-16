4baa3166f7f52b1fb2f4ede24b1909a91646a4f5
정선 <jspark7373@naver.com>
2019-08-16 17:25:38 +0900

    fix bug

--- ddukddak/src/reducers/reducer_code.js
+++ ddukddak/src/reducers/reducer_code.js
@@ -1,6 +1,7 @@
 import {FETCH_CODE} from '../action/index';
 
 export default function(state = null, action){
+    console.log(action.payload);
     switch(action.type){
     case FETCH_CODE:
         return action.payload;
