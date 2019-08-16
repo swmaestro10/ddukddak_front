65ca35ad7c7be9b58693abaa4ebc9b513676c2de
정선 <jspark7373@naver.com>
2019-08-16 22:10:23 +0900

    sdfsd

--- ddukddak/src/components/terminal_container.js
+++ ddukddak/src/components/terminal_container.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import BlockTerminal from '../container/block_terminal';
-// import CodeTerminal from '../container/code_terminal';
+import CodeTerminal from '../container/code_terminal';
 import './terminal_container.css';
 
 export default class TerminalContainer extends Component {
@@ -9,7 +9,7 @@ export default class TerminalContainer extends Component {
         return (
             <div className = "terminal_container">
                 <BlockTerminal />
-                {/* <CodeTerminal / > */}
+                <CodeTerminal / >
             </div>
         );
     }
