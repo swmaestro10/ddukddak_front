87af7557da03015de43021f2f3b08bd3e8b63714
정선 <jspark7373@naver.com>
2019-08-16 22:09:40 +0900

    sdfs

--- ddukddak/src/reducers/reducer_code.js
+++ ddukddak/src/reducers/reducer_code.js
@@ -1,6 +1,6 @@
 import {FETCH_CODE} from '../action/index';
 
-export default function(state = null[], action){
+export default function(state = [], action){
     switch(action.type){
     case FETCH_CODE:
         return action.payload;
