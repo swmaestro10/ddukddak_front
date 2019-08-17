af74e458b37fe7ed0e8e2c7801e234908a779931
정선 <jspark7373@naver.com>
2019-08-18 01:42:04 +0900

    sdfs

--- ddukddak/src/container/tutorial.js
+++ ddukddak/src/container/tutorial.js
@@ -11,7 +11,7 @@ class Tutorial extends React.Component{
 	}
 	updateDiscription = async() => {
 		const result = await axios.get(
-			'http://52.78.238.217/class/enter',
+			'http://ddukddak.io/class/enter',
 			{
 				params : {
 					"token":"Tc5Ly0FLEzgmJ7uU+NpzkedS8FpzL14dFVVvzhN+uaD6Y3iYBLpSD9v8ySsMTVdfAZRpKLpNeht1LeAPeKmNUagxw1y0XbekE95MQ+E5xa85WUuFW2dAkkheceuuBvucVJdvDq9YycIHfSPdJ/Nj2uVFMMxvQlt+TxCjYPNASl21N4bzz/5QvQmqIKeh2fQ8j8D/nwxzKOYItL7S1J3FKi+aeysmvXbgnXX11JvEtr+O2nZ2FFL+g1tWuFhJ1tyzC3IH84oNKMNIMeecdNrQ30GxM3ZTaUg7KPL/06zb81+r3lpE18vVAC6EKQ9oK6t2EeRnIZv8isZmA95d17GpeL+PlraKf3St5YTzXQnCYRJb+Q6rvk7+VaN8vSelQ2E0gbEdrb1VbxhEZn7FPyJzqrhazyTrx+EXFn+MN7GA9c1s7AzNUXSqaA==",
