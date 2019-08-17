1dd686c4bf4e44f60330596723bf55c41f12b723
정선 <jspark7373@naver.com>
2019-08-18 01:34:16 +0900

    sdfs

--- ddukddak/src/container/code_terminal.js
+++ ddukddak/src/container/code_terminal.js
@@ -11,14 +11,13 @@ class CodeTerminal extends Component {
         this.onSubmitCode = this.onSubmitCode.bind(this);
     }
     onSubmitCode= async() => {
-        console.log(`"${this.props.code[0]}"`);
         const result = await axios.get(
             'http://52.78.238.217/class/sub/submit',
             {
                 params : {
                     "token": "Tc5Ly0FLEzgmJ7uU+NpzkedS8FpzL14dFVVvzhN+uaD6Y3iYBLpSD9v8ySsMTVdfAZRpKLpNeht1LeAPeKmNUagxw1y0XbekE95MQ+E5xa85WUuFW2dAkkheceuuBvucVJdvDq9YycIHfSPdJ/Nj2uVFMMxvQlt+TxCjYPNASl21N4bzz/5QvQmqIKeh2fQ8j8D/nwxzKOYItL7S1J3FKi+aeysmvXbgnXX11JvEtr+O2nZ2FFL+g1tWuFhJ1tyzC3IH84oNKMNIMeecdNrQ30GxM3ZTaUg7KPL/06zb81+r3lpE18vVAC6EKQ9oK6t2EeRnIZv8isZmA95d17GpeL+PlraKf3St5YTzXQnCYRJb+Q6rvk7+VaN8vSelQ2E0gbEdrb1VbxhEZn7FPyJzqrhazyTrx+EXFn+MN7GA9c1s7AzNUXSqaA==",
                     "subclass" : "1",
-                    "code" : this.props.code[0]
+                    "code" : `"${this.props.code[0]}"`
                 }
             });
         console.log(result);
--- ddukddak/src/container/tutorial.js
+++ ddukddak/src/container/tutorial.js
@@ -1,21 +1,37 @@
 import React from 'react';
+import axios from 'axios';
 import './tutorial.css';
 
 class Tutorial extends React.Component{
-
+	constructor(props){
+		super(props);
+		
+		this.state = {discription : ""}
+	}
+	updateDiscription = async() => {
+		const result = await axios.get(
+			'http://52.78.238.217/class/enter',
+			{
+				params : {
+					"token":"Tc5Ly0FLEzgmJ7uU+NpzkedS8FpzL14dFVVvzhN+uaD6Y3iYBLpSD9v8ySsMTVdfAZRpKLpNeht1LeAPeKmNUagxw1y0XbekE95MQ+E5xa85WUuFW2dAkkheceuuBvucVJdvDq9YycIHfSPdJ/Nj2uVFMMxvQlt+TxCjYPNASl21N4bzz/5QvQmqIKeh2fQ8j8D/nwxzKOYItL7S1J3FKi+aeysmvXbgnXX11JvEtr+O2nZ2FFL+g1tWuFhJ1tyzC3IH84oNKMNIMeecdNrQ30GxM3ZTaUg7KPL/06zb81+r3lpE18vVAC6EKQ9oK6t2EeRnIZv8isZmA95d17GpeL+PlraKf3St5YTzXQnCYRJb+Q6rvk7+VaN8vSelQ2E0gbEdrb1VbxhEZn7FPyJzqrhazyTrx+EXFn+MN7GA9c1s7AzNUXSqaA==",
+					"class" : "1"
+				}
+			});
+		console.log(result);
+	}
     render(){
         return (
-	<section className="course_container">
-		<div className="course_title">
-			<h1>Machine Learning</h1>
-		</div>
-		<div className="course_tutorial">
-			2
-		</div>
-		<div className="course_progress">
-            3
-		</div>
-	</section>	
+			<section className="course_container">
+				<div className="course_title">
+					<h1>Machine Learning</h1>
+				</div>
+				<div className="course_tutorial">
+					{this.state.discription}
+				</div>
+				<div className="course_progress">
+					3
+				</div>
+			</section>	
 	)
     }
 }
