8a8b51f769dfa2f7d8727b86706d5f7a826d80c1
sunimooni <jspark7373@naver.com>
2019-08-23 17:11:12 +0900

    sdfs

--- ddukddak/src/container/code_terminal.js
+++ ddukddak/src/container/code_terminal.js
@@ -11,14 +11,12 @@ class CodeTerminal extends Component {
         this.onSubmitCode = this.onSubmitCode.bind(this);
     }
     onSubmitCode= async() => {
-        axios.get('http://ddukddak.io/class/sub/submit',{
-                params : {
+        const result = await axios.post('http://ddukddak.io/class/sub/submit',{
                     token : 'Tc5Ly0FLEzgmJ7uU+NpzkedS8FpzL14dFVVvzhN+uaD6Y3iYBLpSD9v8ySsMTVdfAZRpKLpNeht1LeAPeKmNUagxw1y0XbekE95MQ+E5xa85WUuFW2dAkkheceuuBvucVJdvDq9YycIHfSPdJ/Nj2uVFMMxvQlt+TxCjYPNASl21N4bzz/5QvQmqIKeh2fQ8j8D/nwxzKOYItL7S1J3FKi+aeysmvXbgnXX11JvEtr+O2nZ2FFL+g1tWuFhJ1tyzC3IH84oNKMNIMeecdNrQ30GxM3ZTaUg7KPL/06zb81+r3lpE18vVAC6EKQ9oK6t2EeRnIZv8isZmA95d17GpeL+PlraKf3St5YTzXQnCYRJb+Q6rvk7+VaN8vSelQ2E0gbEdrb1VbxhEZn7FPyJzqrhazyTrx+EXFn+MN7GA9c1s7AzNUXSqaA==',
                     subclass : '1',
                     code : `'${this.props.code[0]}'`
-                }
             })
-            .then(response => { console.log(response) })
+            .then(response => { alert('success!') })
             .catch(response => { console.log(response) });
     }
     renderCode() {
@@ -26,7 +24,7 @@ class CodeTerminal extends Component {
 	let codeSplit = codeString.split(';');
 	return codeSplit.map((code) => {
 		if(code !== "")
-			return (<li key={code} className="codes">{code}</li>);
+			return (<li key={code} className="codes">>>> {code}</li>);
 		return "";
 	});
     }
--- ddukddak/src/container/tutorial.js
+++ ddukddak/src/container/tutorial.js
@@ -6,31 +6,37 @@ class Tutorial extends React.Component{
 	constructor(props){
 		super(props);
 		
-		this.state = {discription : ""};
+		this.state = {
+			title : "Loading...",
+			discription : "Loading..."
+		
+		};
 		this.updateDiscription();
 	}
 	updateDiscription = async() => {
-		const result = await axios.get(
-			'http://ddukddak.io/user/signin',
+		const result = await axios.post(
+			'http://ddukddak.io/class/enter',
 			{
-				params : {
-					email : '1@test.com',
-					pw : 'password'
-				}
+				token : 'Tc5Ly0FLEzgmJ7uU+NpzkedS8FpzL14dFVVvzhN+uaD6Y3iYBLpSD9v8ySsMTVdfAZRpKLpNeht1LeAPeKmNUagxw1y0XbekE95MQ+E5xa85WUuFW2dAkkheceuuBvucVJdvDq9YycIHfSPdJ/Nj2uVFMMxvQlt+TxCjYPNASl21N4bzz/5QvQmqIKeh2fQ8j8D/nwxzKOYItL7S1J3FKi+aeysmvXbgnXX11JvEtr+O2nZ2FFL+g1tWuFhJ1tyzC3IH84oNKMNIMeecdNrQ30GxM3ZTaUg7KPL/06zb81+r3lpE18vVAC6EKQ9oK6t2EeRnIZv8isZmA95d17GpeL+PlraKf3St5YTzXQnCYRJb+Q6rvk7+VaN8vSelQ2E0gbEdrb1VbxhEZn7FPyJzqrhazyTrx+EXFn+MN7GA9c1s7AzNUXSqaA==',
+				class : '1'
 			})
-			.then(response => { console.log(response) })
+			.then(response => {
+				this.setState({
+					title : response.data[1].name,
+					discription : response.data[1].text_tutorial
+				});
+			})
 			.catch(response => { console.log(response) });
-		this.setState({discription : result});
 	}
     render(){
         return (
 			<section className="course_container">
 				<div className="course_title">
-					<h1>Machine Learning TEST Class</h1>
+					<h1>{this.state.title}</h1>
 				</div>
 				<div className="course_tutorial">
 					<h4>Step 1 - Training Data</h4>
-					{this.state.discription}	
+					<p>{this.state.discription}</p>
 				</div>
 				<div className="course_progress">
 					<div className="course_button">
